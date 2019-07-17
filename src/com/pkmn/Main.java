b61bbb518a4ba6c4940362aa4d6ddb69bb29b9d7
Brice Beaurepaire <brice@mbmail.fr>
2019-07-17 11:27:10 +0200

    Reading XML

--- src/com/pkmn/Main.java
+++ src/com/pkmn/Main.java
@@ -15,7 +15,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.nio.channels.AlreadyBoundException;
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class Main implements ActionListener, KeyListener
 {
@@ -61,6 +71,34 @@ public class Main implements ActionListener, KeyListener
 	{
 		choice = win.jtf.getText().toUpperCase();
 		win.jtf.setText("");
+		final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try 
+		{
+			final DocumentBuilder builder = factory.newDocumentBuilder();
+			final Document document= builder.parse(new File("src/res/maps/0.xml"));
+			final Element racine = document.getDocumentElement();
+			final NodeList racineNoeuds = racine.getChildNodes();
+			final Element head = (Element) racineNoeuds.item(1);
+			final Element name = (Element) head.getElementsByTagName("name").item(0);
+			System.out.println("Welcome to "+name.getTextContent());
+			final Element texts = (Element) racineNoeuds.item(5);
+			final Element speech = (Element) texts.getElementsByTagName("speech").item(0);
+			System.out.println(speech.getTextContent());
+		} 
+		catch (ParserConfigurationException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (SAXException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
